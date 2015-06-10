fee6b924fddbc253ffa3493e9d13a5aaad9bd93a
Vladimir Yuldashev <misterio92@gmail.com>
2015-06-10 19:56:24 +0300

    remove unneccesary use

--- src/FintechFab/LaravelQueueRabbitMQ/Queue/Jobs/RabbitMQJob.php
+++ src/FintechFab/LaravelQueueRabbitMQ/Queue/Jobs/RabbitMQJob.php
@@ -6,7 +6,6 @@ use Illuminate\Contracts\Queue\Job as JobContract;
 use Illuminate\Queue\Jobs\Job;
 use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Message\AMQPMessage;
-use Queue;
 
 class RabbitMQJob extends Job implements JobContract
 {
