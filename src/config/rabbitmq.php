61cc707e7b1130058428218bcafcf57032f4d10e
Samuel Levy <sam@samuellevy.com>
2016-10-18 08:58:15 +1000

    Added better error handling to rabbitmq connectors
    If the RabbitMQ connector was unavailable, and the queue was running in daemon mode, the error log would flood within seconds. This reports the connection error responsibly, and sleeps to stop the log file from being instantly flooded.

--- src/config/rabbitmq.php
+++ src/config/rabbitmq.php
@@ -33,4 +33,5 @@ return [
         'auto_delete' 		=> env('RABBITMQ_EXCHANGE_AUTODELETE', false),
     ],
 
+    'sleep_on_error'        => env('RABBITMQ_ERROR_SLEEP', 5), // the number of seconds to sleep if there's an error communicating with rabbitmq
 ];
