48b620a57045863d899e30e6ff6086a2bcc90787
Ratko Rudic <ratko.rudic@pop-tv.si>
2017-08-08 15:55:07 +0200

    Queue arguments must be provided as arrays.
    ENV variables cannot have arrays. So we use JSON notation and
    transform it to arrays.

--- config/rabbitmq.php
+++ config/rabbitmq.php
@@ -39,7 +39,7 @@ return [
         'durable'     => env('RABBITMQ_QUEUE_DURABLE', true),
         'exclusive'   => env('RABBITMQ_QUEUE_EXCLUSIVE', false),
         'auto_delete' => env('RABBITMQ_QUEUE_AUTODELETE', false),
-        'arguments'   => env('RABBITMQ_QUEUE_ARGUMENTS', false),
+        'arguments'   => json_decode(env('RABBITMQ_QUEUE_ARGUMENTS', '{}'), 1),
     ],
     'exchange_params' => [
         'name' => env('RABBITMQ_EXCHANGE_NAME', null),
