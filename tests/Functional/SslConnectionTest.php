4f2a5c4810edaad53e01472feaa83acdf5b3b379
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-11-01 16:03:34 +0200

    add test for secure connection.

--- tests/Functional/SslConnectionTest.php
+++ tests/Functional/SslConnectionTest.php
@@ -15,50 +15,53 @@ class SslConnectionTest extends TestCase
 {
     public function testConnectorEstablishSecureConnectionWithRabbitMQBroker()
     {
-//        $config = [
-//            'factory_class' => AmqpConnectionFactory::class,
-//            'dsn'      => null,
-//            'host'     => getenv('HOST'),
-//            'port'     => getenv('PORT_SSL'),
-//            'login'    => 'guest',
-//            'password' => 'guest',
-//            'vhost'    => '/',
-//
-//            'queue'              => 'queue_name',
-//            'exchange_declare'   => true,
-//            'queue_declare'   => true,
-//            'queue_declare_bind' => true,
-//
-//            'queue_params' => [
-//                'passive'     => false,
-//                'durable'     => true,
-//                'exclusive'   => false,
-//                'auto_delete' => false,
-//                'arguments'   => null,
-//            ],
-//            'exchange_params' => [
-//                'name'        => null,
-//                'type'        => AmqpTopic::TYPE_DIRECT,
-//                'passive'     => false,
-//                'durable'     => true,
-//                'auto_delete' => false,
-//            ],
-//            'ssl_params' => [
-//                'cafile'        => getenv('RABBITMQ_SSL_CAFILE')
-//            ]
-//        ];
-//
-//        $connector = new RabbitMQConnector(new Dispatcher());
-//        /** @var RabbitMQQueue $queue */
-//        $queue = $connector->connect($config);
-//
-//        $this->assertInstanceOf(RabbitMQQueue::class, $queue);
-//
-//        /** @var AmqpContext $context */
-//        $context = $queue->getContext();
-//        $this->assertInstanceOf(AmqpContext::class, $context);
-//
-//        $this->assertInstanceOf(AMQPSSLConnection::class, $context->getLibChannel()->getConnection());
-//        $this->assertTrue($context->getLibChannel()->getConnection()->isConnected());
+        $config = [
+            'factory_class' => AmqpConnectionFactory::class,
+            'dsn'      => null,
+            'host'     => getenv('HOST'),
+            'port'     => getenv('PORT_SSL'),
+            'login'    => 'guest',
+            'password' => 'guest',
+            'vhost'    => '/',
+
+            'queue'              => 'queue_name',
+            'exchange_declare'   => true,
+            'queue_declare'   => true,
+            'queue_declare_bind' => true,
+
+            'queue_params' => [
+                'passive'     => false,
+                'durable'     => true,
+                'exclusive'   => false,
+                'auto_delete' => false,
+                'arguments'   => null,
+            ],
+            'exchange_params' => [
+                'name'        => null,
+                'type'        => AmqpTopic::TYPE_DIRECT,
+                'passive'     => false,
+                'durable'     => true,
+                'auto_delete' => false,
+            ],
+            'ssl_params' => [
+                'cafile'        => getenv('RABBITMQ_SSL_CAFILE'),
+                'local_cert'    => null,
+                'verify_peer'   => false,
+                'passphrase'    => null,
+            ]
+        ];
+
+        $connector = new RabbitMQConnector(new Dispatcher());
+        /** @var RabbitMQQueue $queue */
+        $queue = $connector->connect($config);
+
+        $this->assertInstanceOf(RabbitMQQueue::class, $queue);
+
+        /** @var AmqpContext $context */
+        $context = $queue->getContext();
+        $this->assertInstanceOf(AmqpContext::class, $context);
+
+        $this->assertInstanceOf(AMQPSSLConnection::class, $context->getLibChannel()->getConnection());
+        $this->assertTrue($context->getLibChannel()->getConnection()->isConnected());
     }
 }
