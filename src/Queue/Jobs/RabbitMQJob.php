edd9cc43082d2846fe517db30aeb29a03b04b04e
Vladimir Yuldashev <misterio92@gmail.com>
2019-09-04 13:29:30 +0300

    laravel 6

--- src/Queue/Jobs/RabbitMQJob.php
+++ src/Queue/Jobs/RabbitMQJob.php
@@ -9,14 +9,14 @@ use Illuminate\Queue\Jobs\Job;
 use Interop\Amqp\AmqpConsumer;
 use Illuminate\Queue\Jobs\JobName;
 use Illuminate\Container\Container;
-use Illuminate\Database\DetectsDeadlocks;
+use Illuminate\Database\DetectsLostConnections;
 use Illuminate\Contracts\Queue\Job as JobContract;
 use VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue;
 use VladimirYuldashev\LaravelQueueRabbitMQ\Horizon\RabbitMQQueue as HorizonRabbitMQQueue;
 
 class RabbitMQJob extends Job implements JobContract
 {
-    use DetectsDeadlocks;
+    use DetectsLostConnections;
 
     /**
      * Same as RabbitMQQueue, used for attempt counts.
@@ -58,7 +58,7 @@ class RabbitMQJob extends Job implements JobContract
             with($this->instance = $this->resolve($class))->{$method}($this, $payload['data']);
         } catch (Exception $exception) {
             if (
-                $this->causedByDeadlock($exception) ||
+                $this->causedByLostConnection($exception) ||
                 Str::contains($exception->getMessage(), ['detected deadlock'])
             ) {
                 sleep(2);
@@ -136,7 +136,6 @@ class RabbitMQJob extends Job implements JobContract
      * Get the job identifier.
      *
      * @return string
-     * @throws \Interop\Queue\Exception
      */
     public function getJobId(): string
     {
@@ -171,7 +170,7 @@ class RabbitMQJob extends Job implements JobContract
             return unserialize($body['data']['command']);
         } catch (Exception $exception) {
             if (
-                $this->causedByDeadlock($exception) ||
+                $this->causedByLostConnection($exception) ||
                 Str::contains($exception->getMessage(), ['detected deadlock'])
             ) {
                 sleep(2);
