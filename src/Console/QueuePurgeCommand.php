59e1d2cc2e50c8be4268e9d50821a146c325bf58
Vladimir Yuldashev <misterio92@gmail.com>
2019-11-25 18:37:08 +0300

    wip

--- src/Console/QueuePurgeCommand.php
+++ src/Console/QueuePurgeCommand.php
@@ -4,13 +4,17 @@ namespace VladimirYuldashev\LaravelQueueRabbitMQ\Console;
 
 use Exception;
 use Illuminate\Console\Command;
+use Illuminate\Console\ConfirmableTrait;
 use VladimirYuldashev\LaravelQueueRabbitMQ\Queue\Connectors\RabbitMQConnector;
 
 class QueuePurgeCommand extends Command
 {
+    use ConfirmableTrait;
+
     protected $signature = 'rabbitmq:queue-purge
                            {queue}
-                           {connection=rabbitmq : The name of the queue connection to use}';
+                           {connection=rabbitmq : The name of the queue connection to use}
+                           {--force : Force the operation to run when in production}';
 
     protected $description = 'Purge all messages in queue';
 
@@ -20,6 +24,10 @@ class QueuePurgeCommand extends Command
      */
     public function handle(RabbitMQConnector $connector): void
     {
+        if (!$this->confirmToProceed()) {
+            return;
+        }
+
         $config = $this->laravel['config']->get('queue.connections.'.$this->argument('connection'));
 
         $queue = $connector->connect($config);
