c2e990ab367ebc8dd596d52213d0a3233a9a1b0f
Emiel Bom <emiel.bom@webitize.nl>
2020-02-02 01:22:40 +0100

    Cleanup fork

--- src/Queue/Tools/BackoffStrategyAware.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace VladimirYuldashev\LaravelQueueRabbitMQ\Queue\Tools;
-
-interface BackoffStrategyAware
-{
-    public function setBackoffStrategy(BackoffStrategy $backoffStrategy = null);
-}
--- src/Queue/Tools/PrioritizeAware.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace VladimirYuldashev\LaravelQueueRabbitMQ\Queue\Tools;
-
-interface PrioritizeAware
-{
-    public function setPrioritize(?bool $prioritize = null);
-}
