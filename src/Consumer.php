f641ab110a053502dd60364e84fe6ee348df9201
Dmitry <coder1994@gmail.com>
2020-08-25 14:38:40 +0300

    Fix worker getting killed by timeout when no more jobs available

--- src/Consumer.php
+++ src/Consumer.php
@@ -97,6 +97,11 @@ class Consumer extends Worker
                 }
 
                 $this->runJob($job, $connectionName, $options);
+                
+                
+                if ($this->supportsAsyncSignals()) {
+                    $this->resetTimeoutHandler();
+                }
             }
         );
 
