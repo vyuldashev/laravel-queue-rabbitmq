602d9c8b1362c81bd75ae9e5f4d90f47ca503f17
Arthur <arturs.skulteckis@web-design.lv>
2022-12-05 11:31:42 +0200

    Re-add return type on consumer stop

--- src/Consumer.php
+++ src/Consumer.php
@@ -198,7 +198,7 @@ class Consumer extends Worker
      * @param  WorkerOptions|null  $options
      * @return int
      */
-    public function stop($status = 0, $options = null)
+    public function stop($status = 0, $options = null): int
     {
         // Tell the server you are going to stop consuming.
         // It will finish up the last message and not send you any more.
