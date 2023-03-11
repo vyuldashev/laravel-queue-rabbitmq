61f9142d8a09a14626c68c7c71171382092295e3
Emiel Bom <emiel.bom@webitize.nl>
2023-03-11 22:00:33 +0100

    added:   - factory for creating a config object.
    fixed:
      - tests
    
    removed:
      - unnecessary methods

--- src/Queue/QueueFactory.php
+++ src/Queue/QueueFactory.php
@@ -5,11 +5,11 @@ namespace VladimirYuldashev\LaravelQueueRabbitMQ\Queue;
 use Illuminate\Support\Arr;
 use VladimirYuldashev\LaravelQueueRabbitMQ\Horizon\RabbitMQQueue as HorizonRabbitMQQueue;
 
-final class QueueFactory
+class QueueFactory
 {
     public static function make(array $config = []): RabbitMQQueue
     {
-        $queueConfig = self::createQueueConfig($config);
+        $queueConfig = QueueConfigFactory::make($config);
         $worker = Arr::get($config, 'worker', 'default');
 
         if (strtolower($worker) == 'default') {
@@ -21,40 +21,5 @@ final class QueueFactory
         }
 
         return new $worker($queueConfig);
-    }
-
-    /**
-     * Create a config object from config array
-     */
-    private static function createQueueConfig(array $config = []): QueueConfig
-    {
-        return tap(new QueueConfig(), function (QueueConfig $queueConfig) use ($config) {
-            if (! empty($queue = Arr::get($config, 'queue'))) {
-                $queueConfig->setQueue($queue);
-            }
-            if (! empty($afterCommit = Arr::get($config, 'after_commit'))) {
-                $queueConfig->setDispatchAfterCommit($afterCommit);
-            }
-
-            if (! empty($queueOptionsConfig = Arr::get($config, 'options.queue'))) {
-                $queueConfig
-                    ->setAbstractJob(Arr::pull($queueOptionsConfig, 'job'))
-                    // Feature: Prioritize delayed messages.
-                    ->setPrioritizeDelayed(Arr::pull($queueOptionsConfig, 'prioritize_delayed'))
-                    ->setQueueMaxPriority(Arr::pull($queueOptionsConfig, 'queue_max_priority'))
-                    // Feature: Working with Exchange and routing-keys
-                    ->setExchange(Arr::pull($queueOptionsConfig, 'exchange'))
-                    ->setExchangeType(Arr::pull($queueOptionsConfig, 'exchange_type'))
-                    ->setExchangeRoutingKey(Arr::pull($queueOptionsConfig, 'exchange_routing_key'))
-                    // Feature: Reroute failed messages
-                    ->setRerouteFailed(Arr::pull($queueOptionsConfig, 'reroute_failed'))
-                    ->setFailedExchange(Arr::pull($queueOptionsConfig, 'failed_exchange'))
-                    ->setFailedRoutingKey(Arr::pull($queueOptionsConfig, 'failed_routing_key'))
-                    // Feature: Mark queue as quorum
-                    ->setQuorum(Arr::pull($queueOptionsConfig, 'quorum'))
-                    // All extra options not defined
-                    ->setOptions($queueOptionsConfig);
-            }
-        });
     }
 }
