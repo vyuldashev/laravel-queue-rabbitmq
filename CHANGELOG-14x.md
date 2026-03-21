226393ba5f01186d3e657d6da6e5722f8b1ff303
sohrabafard <sohrab.a.fard@gmail.com>
2026-03-21 21:57:05 +0330

    feat: add Laravel 13 and PHP 8.5 support

--- CHANGELOG-14x.md
+++ CHANGELOG-14x.md
@@ -3,6 +3,9 @@
 All notable changes to this project will be documented in this file.
 
 ## [unreleased](https://github.com/vyuldashev/laravel-queue-rabbitmq/compare/v13.3.0...master)
+- Add Laravel 13 compatibility to the queue constraints.
+- Allow PHP 8.5-compatible `php-amqplib/php-amqplib` and PHPUnit 12 in the development matrix.
+- Make Horizon an optional suggestion for fork validation because the stable Horizon line has not declared Laravel 13 support yet.
 
 ## [14.0.0]
 - First release compatible with Laravel 11
--- src/Consumer.php
+++ src/Consumer.php
@@ -197,14 +197,15 @@ class Consumer extends Worker
      *
      * @param  int  $status
      * @param  WorkerOptions|null  $options
+     * @param  mixed  $reason
      * @return int
      */
-    public function stop($status = 0, $options = null)
+    public function stop($status = 0, $options = null, $reason = null)
     {
         // Tell the server you are going to stop consuming.
         // It will finish up the last message and not send you any more.
         $this->channel->basic_cancel($this->consumerTag, false, true);
 
-        return parent::stop($status, $options);
+        return parent::stop($status, $options, $reason);
     }
 }
